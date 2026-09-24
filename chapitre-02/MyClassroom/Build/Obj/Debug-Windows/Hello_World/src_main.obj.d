21dfee18c8d05cd368e68a437c77336c9f4b2093
Toungsi <kevinkountchou@gmail.com>
2026-09-24 10:13:54 +0100

    Add files via upload

---
+++ chapitre-02/MyClassroom/Build/Obj/Debug-Windows/Hello_World/src_main.obj.d
@@ -0,0 +1,2 @@
+C:\Users\All Computer\Desktop\Polytech\S1\VR_AR_XR\chapitre-02\MyClassroom\Build\Obj\Debug-Windows\Hello_World\src_main.obj: \
+  C:/Users/All\ Computer/Desktop/Polytech/S1/VR_AR_XR/chapitre-02/MyClassroom/Hello_World/src/main.cpp
---
+++ chapitre-02/MyClassroom/Hello_World/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Hello_World!" << std::endl;
+    return 0;
+}
